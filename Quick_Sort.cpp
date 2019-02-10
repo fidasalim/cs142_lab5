371a6ffc085c16fe994d3aad38ea6d7964a5f817
fidasalim <42141530+fidasalim@users.noreply.github.com>
2019-02-10 23:29:17 +0530

    Update Quick_Sort.cpp

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -2,50 +2,56 @@
 using namespace std;
 
 //function to do swamping
-void swap(int arr[],int low,int high,int *counter){
-	int pivot = arr[high];
-	if(arr[low]>pivot){
-		int a = arr[low];
-		arr[low] = arr[high-1];
-		arr[high-1] = a;
-		counter = &arr[high];
-		//cout<<*counter<<endl;
-		if(low = high){
-			return ;
-		}
-		return swap(arr,low,high-1,counter);
-	}
-	else {
-		swap (arr,low+1,high,counter);
-	}
-	
+int swap(int arr[],int low,int high,int pivot){
+    while(low !=high){
+        if(low !=high){
+        	if(arr[low]>pivot){
+        		int a = arr[low];
+        		arr[low] = arr[high-1];
+        		arr[high-1] = a;
+        		high--;
+        		
+        	}
+        	else {
+        	    low++;
+        	}
+        }
+    }
+    return high;
+    if (arr[low] == pivot && arr[high] == pivot && low==high){
+            return high;
+    }
+    if(low>high){
+        return 0;
+    }
 }	
-	
-	
-void partition(int arr[],int low,int high){
-	//swap(arr,low,high);
-	int pivot = arr[high];
-	//int b =
-	
+void partition(int arr[],int p,int low,int high){
+	int b = arr[p];
+	arr[p] = arr[high];
+	arr[high] = b;
 }
-
-/*QS(int arr[],int low,int high){
-	int p=high;
-	partition(arr,p,low,high){
-		QS(arr,low,p-1);
-		QS(arr,p+1,high);}
-	}*/
+void QS(int arr[],int low,int high){
+    int pivot = arr[high];
+    int p = swap(arr,low,high,pivot);
+    if (low>=high){
+        return;
+    }
+    else{
+    	partition(arr,p,low,high);
+    	//QS(arr,low,p-1);
+    	QS(arr,p+1,high);
+    }
+}
+	
 int display(int arr[]){
-	for(int i = 0;i<5;i++){
+	for(int i = 0;i<6;i++){
 		cout<<arr[i]<<endl;
 	}
 }
 int main(){
-	int *p;
-	 int arr[5]={2,1,5,4,3};
-	 swap(arr,0,4,p);
+	int p;
+	 int arr[]={6,5,4,3,2,1};
+	 QS(arr,0,5);
 	 display(arr);
-}
-		
-
-
+	 
+}	
